57b6a3bca6c481f716f46b360e59719b881d68e2
Elliot Winkler <elliot.winkler@gmail.com>
2011-09-16 23:38:52 -0600

    Put Runner inside a metaclass so private will work

--- lib/guard/jessie/runner.rb
+++ lib/guard/jessie/runner.rb
@@ -1,20 +1,21 @@
 module Guard
   class Jessie
     module Runner
-      def self.run paths
-        return false if paths.empty?
-        system(jessie_command(paths))
-      end
-
-      private
-        def self.jessie_command(paths)
-          cmd_parts = []
-          cmd_parts << "jessie"
-          cmd_parts << "-f progress"
-          cmd_parts << paths.join(" ")
-
-          cmd_parts.join(" ")
+      class << self
+        def run(paths)
+          return false if paths.empty?
+          system(jessie_command(paths))
         end
+
+        private
+          def jessie_command(paths)
+            cmd_parts = []
+            cmd_parts << "jessie"
+            cmd_parts << "-f progress"
+            cmd_parts << paths.join(" ")
+            cmd_parts.join(" ")
+          end
+      end
     end
   end
 end
