d5339374e907bcd9df698bcca73d0aac1c0c9d63
Elliot Winkler <elliot.winkler@gmail.com>
2011-09-17 20:45:16 -0600

    Growl # examples passed/failed based on last run
    In other words, if you have a spec with 2 examples failing and you fix one of
    them, the Growl message will say "1 example failed (-1)", because 2 examples
    failed before and now only 1 does. If instead you fix both of them, the Growl
    message will say "2 examples passed (+2)", because 0 examples were passing
    before and now 2 do.
    
    The difference between the number of failed/passed examples compared to the last
    run is remembered per file, so if you run another spec and that fails, you might
    see a different number.

--- lib/guard/jessie.rb
+++ lib/guard/jessie.rb
@@ -6,16 +6,21 @@ module Guard
     autoload :Notifier, 'guard/jessie/notifier'
     autoload :Runner,   'guard/jessie/runner'
 
-    def run_all
-      info "Jessie: Running all specs"
-      Runner.run %w[spec]
+    def initialize(*args)
+      super
+      @runner = Runner.new
     end
 
-    def run_on_change paths
+    def run_all
+      info "Jessie: Running all specs"
+      @runner.run %w[spec]
+    end
+
+    def run_on_change(paths)
       paths = paths.select {|path| File.exists?(path) }
       if paths.any?
         info "Jessie: Running #{paths.join(", ")}"
-        Runner.run paths
+        @runner.run(paths)
       end
     end
 
--- lib/guard/jessie/runner.rb
+++ lib/guard/jessie/runner.rb
@@ -1,40 +1,59 @@
 module Guard
   class Jessie
-    module Runner
-      class << self
-        def run(paths)
-          return false if paths.empty?
-          run_command(jessie_command(paths))
-        end
+    class Runner
+      def initialize
+        @files = {}
+      end
 
-        private
-          def run_command(command)
-            output = `#{command} 2>&1`
-            lines = output.split(/\n/)
-            result = lines.last.gsub(/\e\[\d+m/, "").strip  # remove coloring
-            if m = result.match(/^(\d+) examples?(?:, (\d+) failures?)?$/)
-              num_examples, num_failed = m[1].to_i, m[2].to_i
-              title = "Jasmine results"
-              if num_failed > 0
-                message = "#{num_failed} example#{'s' if num_failed != 1} failed"
-                Notifier.failure(title, message)
-                puts output
-              else
-                message = "#{num_examples} example#{'s' if num_examples != 1} passed"
-                Notifier.success(title, message)
-              end
-            else
-              puts output
+      def run(paths)
+        fs = @files[paths.first] if paths.size == 1
+        command = jessie_command(paths)
+        output = run_command(command)
+        fs = handle_results(output, fs)
+        @files[paths.first] = fs if fs and paths.size == 1
+      end
+
+    private
+      def jessie_command(paths)
+        cmd_parts = []
+        cmd_parts << "jessie"
+        cmd_parts << "-f progress"
+        cmd_parts << paths.join(" ")
+        cmd_parts.join(" ")
+      end
+
+      def run_command(command)
+        `#{command} 2>&1`
+      end
+
+      def handle_results(output, fs)
+        lines = output.split(/\n/)
+        result = lines.last.gsub(/\e\[\d+m/, "").strip  # remove coloring
+        if m = result.match(/^(\d+) examples?(?:, (\d+) failures?)?$/)
+          num_examples, num_failed = m[1].to_i, m[2].to_i
+          num_passed = num_examples - num_failed
+          title = "Jasmine results"
+          if num_failed > 0
+            message = "#{num_failed} example#{'s' if num_failed != 1} failed"
+            if fs
+              diff = num_failed - fs[:num_failed]
+              message << " (#{diff > 0 ? '+' : '-'}#{diff.abs})" if diff != 0
             end
+            Notifier.failure(title, message)
+            puts output
+          elsif num_passed > 0
+            message = "#{num_examples} example#{'s' if num_examples != 1} passed"
+            if fs
+              diff = num_passed - fs[:num_passed]
+              message << " (#{diff > 0 ? '+' : '-'}#{diff.abs})" if diff != 0
+            end
+            Notifier.success(title, message)
           end
-
-          def jessie_command(paths)
-            cmd_parts = []
-            cmd_parts << "jessie"
-            cmd_parts << "-f progress"
-            cmd_parts << paths.join(" ")
-            cmd_parts.join(" ")
-          end
+          return {:num_passed => num_passed, :num_failed => num_failed}
+        else
+          puts output
+          return nil
+        end
       end
     end
   end
