952939cc858b5d04e0601f6645abca11c5c3b92f
Elliot Winkler <elliot.winkler@gmail.com>
2011-09-22 18:34:51 -0600

    Show how many examples are pending (if any) when all pass

--- lib/guard/jessie/notifier.rb
+++ lib/guard/jessie/notifier.rb
@@ -12,12 +12,16 @@ module Guard
           notify(title, message, :failed, options)
         end
 
+        def pending(title, message, options={})
+          notify(title, message, :pending, options)
+        end
+
         private
           def notify(title, message, status, options)
             out = Array(options[:to] || [])
             color = (status == :success) ? 32 : 31
             if out.include?(:stdio)
-              puts "\e[#{color}m#{title}: #{message}\e[0m"
+              puts "\e[#{color}m#{title}: #{message.gsub(/\n/, " ").squeeze(" ")}\e[0m"
             end
             if out.include?(:growl)
               ::Guard::Notifier.notify(message, :title => title, :image => status)
--- lib/guard/jessie/runner.rb
+++ lib/guard/jessie/runner.rb
@@ -30,14 +30,33 @@ module Guard
       def handle_results(original_output, fs)
         output = original_output.gsub(/\e\[\d+m/, "").strip  # remove coloring
         lines = output.split(/\n/)
+
         if lines[0] =~ /Jessie failed to start/
           if m = lines[1].match(/^[^:]+: .+$/)
             Notifier.failure("Jessie error", lines[1], :to => :growl)
           end
           puts original_output
-        elsif m = lines[-1].match(/^(\d+) examples?(?:, (\d+) failures?)?$/)
-          num_examples, num_failed = m[1].to_i, m[2].to_i
-          num_passed = num_examples - num_failed
+          return
+        end
+
+        result = lines[-1]
+        if m1 = result.match(/(\d+) examples?/)
+          num_examples = m1[1].to_i
+        else
+          num_examples = 0
+        end
+        if m2 = result.match(/(\d+) failures?/)
+          num_failed = m2[1].to_i
+        else
+          num_failed = 0
+        end
+        if m3 = result.match(/(\d+) pending/)
+          num_pending = m3[1].to_i
+        else
+          num_pending = 0
+        end
+        if m1 or m2 or m3
+          num_passed = num_examples - num_failed - num_pending
           title = "Jasmine results"
           if num_failed > 0
             message = "#{num_failed} example#{'s' if num_failed != 1} failed"
@@ -48,18 +67,23 @@ module Guard
             Notifier.failure(title, message, :to => [:stdio, :growl])
             puts original_output
           elsif num_passed > 0
-            message = "#{num_examples} example#{'s' if num_examples != 1} passed"
+            message = "#{num_passed} example#{'s' if num_passed != 1} passed"
             if fs
               diff = num_passed - fs[:num_passed]
               message << " (#{diff > 0 ? '+' : '-'}#{diff.abs})" if diff != 0
             end
+            if num_pending > 0
+              message << "\n(#{num_pending} others #{num_pending == 1 ? 'is' : 'are'} pending)"
+            end
             Notifier.success(title, message, :to => [:stdio, :growl])
+          elsif num_pending > 0
+            Notifier.pending(title, "All examples are pending", :to => [:stdio, :growl])
           end
           return {:num_passed => num_passed, :num_failed => num_failed}
-        else
-          puts original_output
-          return nil
         end
+
+        puts original_output
+        return nil
       end
     end
   end
